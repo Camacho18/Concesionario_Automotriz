42eac92826cdc6c95a1927082b2baa9ee6c899bb
Camacho18 <luis.camacho@tectijuana.edu.mx>
2020-06-16 12:49:22 -0500

    Report

---
+++ Concesionaria/Controllers/ReportesController.cs
@@ -0,0 +1,30 @@
+﻿using Concesionaria.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//User 
+using Concesionaria.Models.ModelsSupport;
+using Microsoft.SqlServer.Server;
+using Newtonsoft.Json;
+using Concesionaria.AutoComplete;
+
+namespace Concesionaria.Controllers
+{
+    public class ReportesController : Controller
+    {
+        private readonly ConcesionariaEntities db = new ConcesionariaEntities();
+        private readonly DropDownList DropDownLisObject = new DropDownList();
+        private readonly RemoveEspace Remove = new RemoveEspace();
+        private string JsonString = string.Empty;
+        private int IdUsuario, IdSucursal, IdEmpleado, IdCom;
+
+        // GET: Reportes
+        public ActionResult FacturaReport()
+        {
+            List<AccesorioList> m = (from a in db.AccesorioList select a).ToList();
+            return View(m);
+        }        
+    }
+}
